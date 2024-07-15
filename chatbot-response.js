44bd3876c71c59dd6f944d9f98c8be93daf83726
KD <huynhthiainhi2636@gmail.com>
2024-07-15 21:23:55 +0700

    updatechatbot

--- chatbot-response.js
+++ chatbot-response.js
@@ -4,36 +4,68 @@ function getChatbotResponse(userInput) {
     if (userInput.toLowerCase().includes('chào')) {
         response = 'Tôi có thể giúp gì cho bạn?';
     } else if (userInput.toLowerCase().includes('tôi muốn mua củi')) {
-        response = '<br>1. Nhấp [Phân loại] để biết thông tin củi ' +
-            '2. Nhấp [Giá] để biết giá ' + '3. Nhập [Ship] để biết tiền ship';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Nhập [củi bó] hoặc [củi miếng] hoặc [củi thanh tròn] để biết thông tin và giá'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
-    else if (userInput.toLowerCase().includes('củi')) {
-        response = '<br>1. Nhấp [Phân loại] để biết thông tin củi ' +
-            '2. Nhấp [Giá] để biết giá ' + '3. Nhập [Ship] để biết tiền ship';
-    } else if (userInput.toLowerCase().includes('tôi muốn mua')) {
-        response = '<br>1. Nhấp [Phân loại] để biết thông tin củi<br>' +
-            '2. Nhấp [Giá] để biết giá<br>' +
-            '3. Nhập [Ship] để biết tiền ship';
+ else if (userInput.toLowerCase().includes('tôi muốn mua')) {
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('thông tin')) {
-        response = '<br>1. Nhấp [Phân loại] để biết thông tin củi<br>' +
-            '2. Nhấp [Giá] để biết giá<br>' +
-            '3. Nhập [Ship] để biết tiền ship';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('bán thế nào')) {
-        response = '<br>1. Nhấp [Phân loại] để biết thông tin củi<br>' +
-            '2. Nhấp [Giá] để biết giá<br>' +
-            '3. Nhập [Ship] để biết tiền ship';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('muốn mua')) {
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('bán củi')) {
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('củi pizza')) {
-        response = '<br>1. Nhấp [Phân loại] để biết thông tin củi<br>' +
-            '2. Nhấp [Giá] để biết giá<br>' +
-            '3. Nhập [Ship] để biết tiền ship';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('sỉ')) {
-        response = '<br>1. Nhấp [Phân loại] để biết thông tin củi<br>' +
-            '2. Nhấp [Giá] để biết giá<br>' +
-            '3. Nhập [Ship] để biết tiền ship';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('vận chuyển')) {
         response = '100K cho bán kính dưới 5km<br>' +
@@ -44,48 +76,141 @@ function getChatbotResponse(userInput) {
             '200K cho bán kính dưới 10km<br>' + 'trên 10km thương lượng';
     }
     else if (userInput.toLowerCase().includes('phân loại')) {
-        response = 'Bên mình chỉ bán củi dương liễu<br>' +
-            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:30%;">' +
-            '<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:30%;">' +
-            '<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:30%;">' + '<br>'+
-        'Giá các loại củi trên như nhau ạ !'
-        '<br>Nhập [giá] để tham khảo giá';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('loại củi')) {
-        response = 'Bên mình chỉ bán củi dương liễu<br>' +
-            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:30%;">' +
-            '<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:30%;">' +
-            '<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:30%;">' + '<br>'
-        'Giá các loại củi trên như nhau ạ !'
-        '<br>Nhập [giá] để tham khảo giá';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('bán')) {
-        response = 'Bên mình chỉ bán củi dương liễu<br>' +
-        '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:30%;">' +
-        '<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:30%;">' +
-        '<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:30%;">' + '<br>'+
-    'Giá các loại củi trên như nhau ạ !'
-    '<br>Nhập [giá] để tham khảo giá';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('loại')) {
-        response = 'Bên mình chỉ bán củi dương liễu<br>' +
-        '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:30%;">' +
-        '<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:30%;">' +
-        '<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:30%;">' + '<br>'+
-    'Giá các loại củi trên như nhau ạ !'
-    '<br>Nhập [giá] để tham khảo giá';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('bán thế nào')) {
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('củi khác không')) {
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('loại củi khác')) {
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('củi dương liễu')) {
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
     else if (userInput.toLowerCase().includes('bán như thế nào')) {
         response = 'Bên mình chỉ bán củi dương liễu. Nhập [giá] để tham khảo giá';
     }
     else if (userInput.toLowerCase().includes('loại củi')) {
-        response = 'Bên mình chỉ bán củi dương liễu<br>' +
-        '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:30%;">' +
-        '<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:30%;">' +
-        '<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:30%;">' + '<br>'+
-    'Giá các loại củi trên như nhau ạ !'
-    '<br>Nhập [giá] để tham khảo giá';
+        response = 'Bên mình bán củi dương liễu<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'
+            +'<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'
+            +'<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
     }
+    else if (userInput.toLowerCase().includes('củi bó')) {
+        response = 'Củi bó giá 11K<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'+
+        'Giá các loại củi trên như nhau ạ !'+
+        '<br>Nhập [giá] để tham khảo giá'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('tôi muốn mua củi bó')) {
+        response = 'Củi bó giá 11.000VND<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('tôi muốn mua củi miếng')) {
+        response = 'Củi miếng 11.000VND<br>' +
+            '<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('tôi muốn mua thanh tròn ')) {
+        response = 'Củi cây tròn 11.000VND<br>' +
+            '<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+
+    else if (userInput.toLowerCase().includes('mua củi bó')) {
+        response = 'Củi bó giá 11.000VND<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('mua củi miếng')) {
+        response = 'Củi miếng 11.000VND<br>' +
+            '<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('mua thanh tròn ')) {
+        response = 'Củi cây tròn 11.000VND<br>' +
+            '<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+
+
+
+    else if (userInput.toLowerCase().includes('mua củi bó')) {
+        response = 'Củi bó giá 11.000VND<br>' +
+            '<img src="img/cuibo.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi bó(hơn 2kg)<br>'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('củi miếng')) {
+        response = 'Củi miếng 11.000VND<br>' +
+            '<img src="img/cuicaylon.jpg" alt="Củi dương liễu" style="width:50%;">' +'<br>củi miếng(hơn 2kg)<br>'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('thanh tròn ')) {
+        response = 'Củi cây tròn 11.000VND<br>' +
+            '<img src="img/cuicaytron.jpg" alt="Củi dương liễu" style="width:50%;">' + '<br>'+ 'củi thanh tròn(hơn 2kg)'+
+        '<br>Nhập [giá] để tham khảo giá các loại khác'+'<br>Nhập [Ship] để biết tiền ship';
+    }
+    else if (userInput.toLowerCase().includes('tôi lấy loại này')) {
+        response = 'Xin lỗi , nhờ bạn nhập tên : Củi Bó, Củi miếng hoặc củi cây hoặc chat trực tiếp với mình' ;
+    }
+    
+
+
 
     else if (userInput.toLowerCase().includes('giá')) {
         response = '<br> Giá 11.000VND/1 cây cho số lượng dưới 150 cây<br>' + 'Giá 10.000VND/1 cây cho số lượng trên 150 cây<br>' + 'Trên 200 cây giá 9700VND/cây ';
@@ -170,7 +295,11 @@ function getChatbotResponse(userInput) {
     else if (userInput.toLowerCase().includes('address')) {
         response = '<br>Số 02 đường 2 tháng 9 ( sau lưng chùa Tĩnh Hội) . Tới liên hệ 0934805005 giá mình trước 1 giờ. Cám ơn bạn';
 
-    } else {
+    }
+    else if (userInput.toLowerCase().includes('tươi')) {
+        response = '<br>Củi phơi ngoài bãi với thời tiết hiện tại thì khô ạ !';
+
+    }  else {
         response = '<br>Xin lỗi, tôi không hiểu bạn muốn nói gì. Bạn có thể hỏi lại không?';
     }
 
--- chatbot.js
+++ chatbot.js
@@ -1,62 +1,78 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const chatBox = document.getElementById('chat-box');
-    const chatInput = document.getElementById('chat-input');
-    const sendButton = document.getElementById('send-button');
+document.addEventListener("DOMContentLoaded", function () {
+  
+  const chatBox = document.getElementById("chat-box");
+  const chatInput = document.getElementById("chat-input");
+  const sendButton = document.getElementById("send-button");
 
-    sendButton.addEventListener('click', function() {
-        const userInput = chatInput.value.trim();
-        if (userInput) {
-            addMessageToChatBox('User', userInput, 'user');
-            const response = getChatbotResponse(userInput);
-            addMessageToChatBox('Chatbot', response, 'bot');
-            chatInput.value = '';
-        }
-    });
-
-    chatInput.addEventListener('keypress', function(event) {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            sendButton.click();
-        }
-    });
-
-    function addMessageToChatBox(sender, message, messageType) {
-        const messageElement = document.createElement('div');
-        messageElement.className = `chat-message ${messageType}`;
-        messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
-        chatBox.appendChild(messageElement);
-        chatBox.scrollTop = chatBox.scrollHeight;
+  sendButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định
+    const userInput = chatInput.value.trim();
+    if (userInput) {
+      addMessageToChatBox("User", userInput, "user");
+      const response = getChatbotResponse(userInput);
+      addMessageToChatBox("Chatbot", response, "bot");
+      chatInput.value = "";
     }
+  });
 
-    function showInitialOptions() {
-        if (!document.getElementById('initial-options')) {
-            const initialOptions = document.createElement('div');
-            initialOptions.id = 'initial-options';
-            initialOptions.className = 'chat-message bot';
-            initialOptions.innerHTML = `
+  const buyButton = document.getElementById("buy-button");
+
+  buyButton.addEventListener("click", function () {
+    showOrderForm(); // Đây là hàm hiển thị form đặt hàng, bạn cần định nghĩa hàm này trong file JavaScript của bạn
+  });
+  chatInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      sendButton.click();
+    }
+  });
+
+
+  function addMessageToChatBox(sender, message, messageType) {
+    const messageElement = document.createElement("div");
+    messageElement.className = `chat-message ${messageType}`;
+    messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+    chatBox.appendChild(messageElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+
+  function showInitialOptions() {
+    if (!document.getElementById("initial-options")) {
+      const initialOptions = document.createElement("div");
+      initialOptions.id = "initial-options";
+      initialOptions.className = "chat-message bot";
+      initialOptions.innerHTML = `
                 <strong>Chatbot:</strong> Bạn muốn làm gì?
-                <div class="mt-2">
+                <div class="mt-2 ">
                     <button id="chat-with-robot" class="btn btn-primary mr-2">Chat với Robot</button>
-                    <button id="chat-directly" class="btn btn-secondary mr-2">Chat Trực Tiếp</button>
-                    <button id="buy-now" class="btn btn-success">Mua Hàng</button>
+                    <button id="chat-directly"  style="float: right;" class="btn btn-secondary mr-2">Chat Trực Tiếp</button>
+                
                 </div>
             `;
-            chatBox.appendChild(initialOptions);
-            chatBox.scrollTop = chatBox.scrollHeight;
+      chatBox.appendChild(initialOptions);
+      chatBox.scrollTop = chatBox.scrollHeight;
 
-            document.getElementById('chat-with-robot').addEventListener('click', function() {
-                addMessageToChatBox('Chatbot', 'Bạn đã chọn chat với robot. Hãy bắt đầu trò chuyện!', 'bot');
-            });
+      document
+        .getElementById("chat-with-robot")
+        .addEventListener("click", function () {
+          addMessageToChatBox(
+            "Chatbot",
+            "Bạn đã chọn chat với robot. Hãy bắt đầu trò chuyện!",
+            "bot"
+          );
+        });
 
-            document.getElementById('chat-directly').addEventListener('click', function() {
-                window.open('https://m.me/cuidanang', '_blank');
-            });
+      document
+        .getElementById("chat-directly")
+        .addEventListener("click", function () {
+          window.open("https://m.me/cuidanang", "_blank");
+        });
 
-            document.getElementById('buy-now').addEventListener('click', function() {
-                showOrderForm();
-            });
-        }
+      document.getElementById("buy-now").addEventListener("click", function () {
+        showOrderForm();
+      });
     }
+  }
 
-    showInitialOptions();
+  showInitialOptions();
 });
